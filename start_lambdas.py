1ad4977d86b95d8683c3da1deb90cd5dc642f389
Piotr Pasternak <piotrpassternak@gmail.com>
2020-11-18 14:02:48 +0100

    Refactor + add reduce

--- start_lambdas.py
+++ start_lambdas.py
@@ -10,54 +10,81 @@ def wait_for_complition(function_name):
             break
         
         if client.get_function(FunctionName=function_name)['Configuration']['State'] == 'Failed':
-            raise Exception("Function failed!!!") 
+            raise Exception("Function" + function_name + "failed!!!") 
             
-        time.sleep(1)    
+        time.sleep(1)
+        
+def map(login, password, scripts, eos_paths, eos_filenames, s3_buckets, s3_object_keys, client):
 
-def start_lambdas(conf_file):
-    
-    #Read data from configuration file
-    conf = json.loads(conf_file)
-
-    client = boto3.client('lambda')
-
-    for eos_path, eos_file, bucket_name, object_key in conf['eos_paths'], conf['eos_filenames'], conf['s3_bucket_names'], conf['s3_object_keys']:
+    for eos_path, eos_file, object_key in eos_paths, eos_filenames, s3_object_keys:
         client.invoke(
             FunctionName = "eos_lambda",
             InvocationType = "Event",
             Payload = {
-                eos_path: eos_url,
+                eos_path: eos_path,
                 eos_filename: eos_file,
-                eos_login: conf['credentials']['login'],
-                eos_password: conf['credentials']['password'],
-                s3_bucket_name: bucket_name,
+                eos_login: login,
+                eos_password: password,
+                s3_bucket_name: s3_buckets[0],
                 s3_object_key: object_key
-                
             }
         )
     
-    wait_for_complition("eos_lambda")
+    try:
+        wait_for_complition("eos_lambda")
+    except:
+        print("Function eos_lambda failed, exiting program...")
+        exit(1)
     
     
-    for script, in_bucket_name, object_key, out_bucket_name in conf['scripts'], conf['s3_bucket_names'], conf['s3_object_keys'], conf['s3_out_bucket_names']:
+    for script, object_key in scripts, s3_object_keys:
         client.invoke(
             FunctionName = "root_lambda",
             InvocationType = "Event",
             Payload = {
-              in_bucket: in_bucket_name,
+              in_bucket: s3_buckets[0],
               in_bucket_file_path: object_key,
-
               script: script,
-
-              out_bucket:out_bucket_name,
+              out_bucket: s3_buckets[1],
               out_bucket_file_path: object_key
-}
+            }
         )
+        
+    try:
+        wait_for_complition("root_lambda")
+    except:
+        print("Function root_lambda failed, exiting program...")
+        exit(1)
+        
+def reduce(bucket):
     
-    wait_for_complition("root_lambda")
+    request = client.invoke(
+                  FunctionName = "reduce_lambda",
+                  InvocationType = "RequestResponse",
+                  Payload = {
+                    bucket: bucket
+                  }
+              )
+
+    return json.loads(request)['result']
+    
+
+def start_lambdas(conf_file, s3_buckets):
+    
+    #Read data from configuration file
+    conf = json.loads(conf_file)
+    
+    login = conf['credentials']['login']
+    password = conf['credentials']['password']
+    scripts = conf['scripts']
+    eos_paths = conf['eos_paths']
+    eos_filenames = conf['eos_filenames']
+    s3_object_keys = conf['s3_object_keys']
+
+    client = boto3.client('lambda')
+
+    map(login, password, scripts, eos_paths, eos_filenames, s3_buckets, s3_object_keys, client)
+    
+    result = reduce(s3_buckets[1])
     
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Success')
-    }
